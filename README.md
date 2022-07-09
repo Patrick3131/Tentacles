fe77b9df7a92c13833d89d9481cd304f9be9375d
Patrick3131 <patrick-fischer@live.de>
2022-07-09 17:03:09 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PFActivityTracking
+
+A description of this package.
---
+++ Sources/PFActivityTracking/PFActivityTracking.swift
@@ -0,0 +1,6 @@
+public struct PFActivityTracking {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
