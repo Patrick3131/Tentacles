70c90908f34fd965ddbb9e9fb75a8f5d552e908a
Patrick3131 <patrick-fischer@live.de>
2022-07-12 01:16:59 +0200

    Renames Adds PFTracking

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "PFActivityTracking",
+    name: "PFTracking",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PFActivityTracking",
-            targets: ["PFActivityTracking"]),
+            name: "PFTracking",
+            targets: ["PFTracking"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PFActivityTracking",
+            name: "PFTracking",
             dependencies: []),
         .testTarget(
-            name: "PFActivityTrackingTests",
-            dependencies: ["PFActivityTracking"]),
+            name: "PFTrackingTests",
+            dependencies: ["PFTracking"]),
     ]
 )
