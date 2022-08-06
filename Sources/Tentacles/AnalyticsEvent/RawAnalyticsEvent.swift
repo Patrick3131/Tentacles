e20ddf27f0f7b23e6efb324d4bb884e0b88d40f2
Patrick3131 <patrick-fischer@live.de>
2022-08-06 18:27:02 +0200

    Adds helper function to easier access attributes of RawAnalyticsEvent Finishes tests for ValuePropositionSession

--- Sources/Tentacles/AnalyticsEvent/RawAnalyticsEvent.swift
+++ Sources/Tentacles/AnalyticsEvent/RawAnalyticsEvent.swift
@@ -9,6 +9,10 @@ import Foundation
 
 /// Event reported to ``AnalyticsReporter`.
 public struct RawAnalyticsEvent {
+    enum Error: Swift.Error {
+        case keyNotAvailable
+        case attributeValueWrongType
+    }
     /// The name of the event being reported.
     public let name: String
     /// Containing all additional attributes that are reported.
@@ -26,5 +30,63 @@ extension RawAnalyticsEvent {
             attributes[key] = value
         }
         self.attributes = attributes
+    }
+}
+
+public extension RawAnalyticsEvent {
+    func getValueAttribute<T>(for key: String) throws -> T {
+        let value: AnyHashable = try getValue(in: self.attributes, for: key)
+        return try downcast(value)
+    }
+    
+    /// Used to get a value from a dic of type Anyhashable.
+    ///
+    /// Downcasts a dic of type AnyHashable to [String: AnyHashable], checks for a value of a key in dic
+    /// and downcasts to type T. In case dic is not of type AnyHashable it will fail.
+    /// Can be used to access nested attributes when transforming ``RawAnalyticsEvent`` with
+    /// ``Middleware``.
+    ///
+    /**
+     struct ExampleAttributes: TentaclesAttributes {
+         struct Key {
+             static let stringProperty: String = "stringProperty"
+             static let nestedProperty: String = "nestedProperty"
+         }
+         struct Nested: Encodable {
+             let stringProperty: String
+         }
+         let stringProperty: String
+         let nestedProperty: Nested
+     }
+     */
+    /// Access:
+    /**
+    ```
+     let nestedProperty: [String: AnyHashable] = try event.getValueAttribute(
+         for: ExampleAttributes.Key.nestedProperty)
+     let nestedStringProperty: String = try event.getValue(in: nestedProperty,
+         for: ExampleAttributes.Key.stringProperty)
+    ```
+    */
+    ///
+    /// - Parameters:
+    ///     -  dic: A dictionary of type AnyHashable
+    ///     - key: Key used in dic
+    /// - Returns:
+    ///     Value of type T
+    func getValue<T>(in dic: AnyHashable,
+                     for key: String) throws -> T {
+        let dic: [String: AnyHashable] = try downcast(dic)
+        guard let value = dic[key] else {
+            throw Error.keyNotAvailable
+        }
+        return try downcast(value)
+    }
+    
+    func downcast<T>(_ value: AnyHashable) throws -> T {
+        guard let typedValue = value as? T else {
+            throw Error.attributeValueWrongType
+        }
+        return typedValue
     }
 }
--- Tests/TentaclesTests/ValuePropositionSessionTests.swift
+++ Tests/TentaclesTests/ValuePropositionSessionTests.swift
@@ -71,11 +71,55 @@ final class ValuePropositionSessionTests: XCTestCase {
     
     func testMakeRawAnalyticsEventWithAction() throws {
         let actionAttributes = KeyValueAttribute(key: "testKey", value: 123)
-        
+        let action = ValuePropositionAction(status: .open, trigger: TentaclesEventTrigger.screenDidAppear, attributes: actionAttributes)
+        let event = session.makeRawAnalyticsEvent(action: action)
+        try evaluateDefaultValues(event: event, status: .opened)
+        /// values added by action
+        let stringProperty: String = try event.getValueAttribute(
+            for: TentaclesAttributesStub.Key.stringProperty)
+        let doubleProperty: Double = try event.getValueAttribute(
+            for: TentaclesAttributesStub.Key.doubleProperty)
+        let boolProperty: Bool = try event.getValueAttribute(
+            for: TentaclesAttributesStub.Key.boolProperty)
+        let enumProperty: String = try event.getValueAttribute(
+            for: TentaclesAttributesStub.Key.enumProperty)
+        let nestedProperty: [String: AnyHashable] = try event.getValueAttribute(
+            for: TentaclesAttributesStub.Key.nestedProperty)
+        let nestedStringProperty: String = try event.getValue(
+            in: nestedProperty,
+            for: TentaclesAttributesStub.Key.stringProperty)
+        let nestedDoubleProperty: Double = try event.getValue(
+            in: nestedProperty,
+            for: TentaclesAttributesStub.Key.doubleProperty)
+        let nestedBoolProperty: Bool = try event.getValue(
+            in: nestedProperty,
+            for: TentaclesAttributesStub.Key.boolProperty)
+        XCTAssertEqual(stringProperty, TentaclesAttributesStub.stringPropertyValue)
+        XCTAssertEqual(doubleProperty, TentaclesAttributesStub.doublePropertyValue)
+        XCTAssertEqual(boolProperty, TentaclesAttributesStub.boolPropertyValue)
+        XCTAssertEqual(enumProperty, TentaclesAttributesStub.enumPropertyValue.rawValue)
+        XCTAssertEqual(nestedStringProperty, TentaclesAttributesStub.Nested.stringPropertyValue)
+        XCTAssertEqual(nestedDoubleProperty, TentaclesAttributesStub.Nested.doublePropertyValue)
+        XCTAssertEqual(nestedBoolProperty, TentaclesAttributesStub.boolPropertyValue)
     }
     
     func testMakeRawAnayticsEventWithTrigger() throws {
-        
+        let event = session.makeRawAnalyticsEvent(trigger: TentaclesEventTrigger.screenDidAppear)
+        try evaluateDefaultValues(event: event, status: .opened)
+    }
+    
+    func evaluateDefaultValues(event: RawAnalyticsEvent,
+                               status: ValuePropositionSession.Status)
+    throws {
+        let _status: String = try event.getValueAttribute(for: KeyAttributes.status)
+        let trigger: String = try event.getValueAttribute(for: KeyAttributes.trigger)
+        let category: String = try event.getValueAttribute(for: KeyAttributes.category)
+        let uuid: String = try event.getValueAttribute(for: KeyAttributes.valuePropositionSessionUUID)
+        XCTAssertNotNil(UUID(uuidString: uuid))
+        XCTAssertEqual(_status, status.rawValue)
+        XCTAssertEqual(trigger, TentaclesEventTrigger.screenDidAppear.rawValue)
+        XCTAssertEqual(category, TentaclesEventCategory.valueProposition.rawValue)
+        XCTAssertEqual(event.name, ValuePropositionStub.name)
     }
     
     private func statusIsMoreRecent(
@@ -96,10 +140,7 @@ final class ValuePropositionSessionTests: XCTestCase {
     private func getTimestamp(for status: ValuePropositionSession.Status,
                               in event: RawAnalyticsEvent)
     throws -> Double {
-        guard let timestamp = (event.attributes[status.rawValue] as? Double) else {
-            throw NSError(domain: "Timestamp for \(status) status not available", code: 0)
-        }
-        return timestamp
+        return try event.getValueAttribute(for: status.rawValue)
     }
     
     private func getValuePropositionSessionUUID()
@@ -110,39 +151,6 @@ final class ValuePropositionSessionTests: XCTestCase {
     
     private func getValuePropositionSessionUUID(in event: RawAnalyticsEvent)
     throws -> String {
-        guard let uuid = (event.attributes[KeyAttributes.valuePropositionSessionUUID] as? String) else {
-            throw NSError(domain: "UUID not available, this might not be a event derived by a ValueProposition", code: 1)
-        }
-        return uuid
+        try event.getValueAttribute(for: KeyAttributes.valuePropositionSessionUUID)
     }
-    
-    private func getValue<T>(in event: RawAnalyticsEvent,
-                             for key: String) throws -> T {
-        guard let value = event.attributes[key] else {
-            throw NSError(domain: "\(key) not available in \(event)", code: 2)
-        }
-        return try downcast(value)
-        
-    }
-    
-    private func downcast<T>(_ value: AnyHashable) throws -> T {
-        guard let typedValue = value as? T else {
-            throw NSError(domain: "\(value) is not of type \(T.self)", code: 3)
-        }
-        return typedValue
-    }
-    
-    private func getValue<T>(in dic: AnyHashable,
-                             for key: String) throws -> T {
-        let dic: [String: AnyHashable] = try downcast(dic)
-        return try downcast(dic)
-    }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
